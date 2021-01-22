99f8dc0ebb436c68a2d88521321c21df4e7edbaa
Qweest <daniilgera1@gmail.com>
2021-01-22 18:27:09 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,25 @@
+importScripts('js/helpers.js', 'js/constants.js');
+
+chrome.runtime.onInstalled.addListener(async () => {
+    await groupService.setStorage(groupService.TITLES_KEY, []);
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    const { groupId, status } = tab;
+
+    if (groupId === GROUP_ID_NONE || status !== TAB_STATUSES.complete) {
+        return;
+    }
+
+    const windowGroups = await groupService.getWindowGroups();
+    const { title } = windowGroups.find((it) => it.id === groupId);
+    const storageGroup = await groupService.getStorageGroup(title);
+
+    // if (storageGroup) {
+    //     await groupService.saveGroup(title);
+    // }
+});
+
+chrome.tabs.onRemoved.addListener(async (tabId) => {
+
+});
